94950a0568cea196195a966581d5cb3203146350
arthr <arthrmrs@gmail.com>
2025-12-03 05:50:01 -0300

    chore: bump version to 0.5.1
    - Atualiza CHANGELOG.md com fix de rotas SPA
    - Correção crítica: 404 ao acessar rotas diretamente

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -5,6 +5,6 @@
  * Sincronizado com package.json
  */
 
-export const APP_VERSION = '0.5.0'
+export const APP_VERSION = '0.5.1'
 export const APP_NAME = 'JoWk - Term.ooo Clone'
 export const BUILD_DATE = '2025-12-03'
